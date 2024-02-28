5a7aa11ebca60208658a5f88bf48d2c5e6a80bfb
JShoon <jsh021813@gmail.com>
2024-02-28 17:23:37 +0900

    feat: admin(구글 로그인) 페이지 css 추가 및 모바일 미디어쿼리 작성, 랜딩페이지 헤더 css 추가 fix: 랜딩페이지 css 하드코딩 삭제

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,12 +1,13 @@
 import GoogleLoginButton from '@src/Components/Googlelogin/GoogleLoginButton'
+import './admin.css'
 
 // 구글 로그인을 위한 페이지
 
 const Admin = () => {
   return (
-    <div id="admin" style={{ margin: '100px' }}>
-      <div style={{ marginBottom: '20px' }}>관리자 로그인을 위한 화면</div>
-      <div style={{ width: '700px' }}>
+    <div id="admin" className="admin">
+      <div className="adminTitle">관리자 로그인을 위한 화면</div>
+      <div>
         <GoogleLoginButton />
       </div>
     </div>
